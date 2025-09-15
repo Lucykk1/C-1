0abc63ac35537922d80da9c3637a18631a18a632
Lucykk1 <lusinekazaryan07@gmail.com>
2025-09-15 10:37:18 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ int main() {
+int year=2024;
+double pi= 3.14159;
+char grade= 'A';
+bool isRainy= true;
+float price= 99.99;
+int number = 50;
+int a, b, c;
+double width =15.5 ;
+double height = 27.5;
+char initial;
+bool isGameover = false;
+ std::cout<<year<<std::endl;
+  return 0;
+}
