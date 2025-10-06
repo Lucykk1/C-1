6ae71106a0f701c70010108074b67263efe2b14a
Lucykk1 <lusinekazaryan07@gmail.com>
2025-10-06 11:45:11 +0300

    Create urok06_1.cpp

---
+++ urok06_1.cpp
@@ -0,0 +1,303 @@
+#include  <iostream>
+
+void task_1() {
+
+    std::cout<<"Enter the number: "<<std::endl;
+    int num=0;
+    std::cin>>num;
+    switch (num){
+        case 1: 
+              std::cout<<"Mn"<<std::endl;
+             break;
+        case 2: 
+              std::cout<<"tu"<<std::endl;      
+              break; 
+        case 3:
+              std::cout<<"wn"<<std::endl;      
+            break;
+        case 4:
+              std::cout<<"th"<<std::endl;
+             break;
+        case 5: 
+              std::cout<<"fr"<<std::endl;
+            break;
+        case 6: 
+              std::cout<<"su"<<std::endl;
+                break;
+        case 7: 
+              std::cout<<"st"<<std::endl;
+                break;
+        default:
+             std::cout<<"error!"<<std::endl;
+    }
+}
+void task_2(){
+ std::cout<<"enter simbol"<<std::endl;
+ char a;
+ switch(a){
+        case '/': 
+              std::cout<<"Delenie"<<std::endl;
+             break;
+        case '*': 
+              std::cout<<"multiplication"<<std::endl;      
+              break; 
+        case :
+              std::cout<<"not bad"<<std::endl;      
+            break;
+        case 4:
+              std::cout<<"good"<<std::endl;
+             break;
+        case 5: 
+              std::cout<<"Very good!"<<std::endl;
+            break;
+       default:
+              std::cout<<"ERRooorrrrrrrr..........."<<std::endl;
+    
+    
+    
+    
+    
+    
+}
+void task_3(){
+    std::cout<<"Enter the grade: "<<std::endl;
+    int num=0;
+    std::cin>>num;
+    switch(num){
+         case 1: 
+              std::cout<<"Very bad"<<std::endl;
+             break;
+        case 2: 
+              std::cout<<"BAD!"<<std::endl;      
+              break; 
+        case 3:
+              std::cout<<"not bad"<<std::endl;      
+            break;
+        case 4:
+              std::cout<<"good"<<std::endl;
+             break;
+        case 5: 
+              std::cout<<"Very good!"<<std::endl;
+            break;
+       default:
+              std::cout<<"ERRooorrrrrrrr..........."<<std::endl;
+        
+    }
+}
+void task_4(){
+    int a=9;
+    int b=3;
+    std::cout<<"Enter the operation: "<<std::endl;
+    char n;
+    std::cin>>n;
+    switch(n){
+         case '*': 
+              std::cout<<a*b<<std::endl;
+             break;
+        case '+': 
+              std::cout<<a+b<<std::endl;      
+              break; 
+        case '/':
+              std::cout<<a/b<<std::endl;      
+            break;
+        case '-':
+              std::cout<<a-b<<std::endl;
+             break;
+      
+       default:
+              std::cout<<"ERRooorrrrrrrr..........."<<std::endl;
+    }
+    
+}
+void task_5(){
+    
+    std::cout<<"Enter the number of mounth: "<<std::endl;
+    int n;
+    std::cin>>n;
+    switch (n){
+        case 1: 
+              std::cout<<"31"<<std::endl;
+             break;
+        case 2: 
+              std::cout<<"28"<<std::endl;      
+              break; 
+        case 3:
+              std::cout<<"31"<<std::endl;      
+            break;
+        case 4:
+              std::cout<<"30"<<std::endl;
+             break;
+        case 5: 
+              std::cout<<"31"<<std::endl;
+            break;
+        case 6: 
+              std::cout<<"30"<<std::endl;
+                break;
+        case 7: 
+              std::cout<<"31"<<std::endl;
+                break;
+              break;
+        case 8: 
+              std::cout<<"31"<<std::endl;
+            break;
+        case 9: 
+              std::cout<<"30"<<std::endl;
+                break;
+        case 10: 
+              std::cout<<"31"<<std::endl;
+                break;
+        case 11: 
+              std::cout<<"30"<<std::endl;
+            break;
+        case 12: 
+              std::cout<<"31"<<std::endl;
+                break;
+       
+        default:
+             std::cout<<"error!"<<std::endl;
+    }
+}
+
+void task_6(){
+    std::cout<<"Enter the latter: "<<std::endl;
+    int n;
+    std::cin>>n;
+    switch (n){
+        case 'R': 
+              std::cout<<"Russia"<<std::endl;
+             break;
+        case 'U': 
+              std::cout<<"USA"<<std::endl;      
+              break; 
+        case 'A':
+              std::cout<<"Armenia"<<std::endl;      
+            break;
+        case 'P':
+              std::cout<<"Poland"<<std::endl;
+             break;
+         
+        default:
+             std::cout<<"error!"<<std::endl;
+       
+    }
+}
+
+void task_7(){
+    std::cout<<"Enter the num"<<std::endl;
+    int n;
+    std::cin>>n; 
+    switch (n){
+          case 1: 
+              std::cout<<"Start"<<std::endl;      
+              break; 
+        case 2 :
+              std::cout<<"Setting"<<std::endl;      
+            break;
+        case 3:
+              std::cout<<"Exit"<<std::endl;
+             break;
+      default:
+             std::cout<<"error!"<<std::endl;
+}
+      }
+      
+void task_8(){
+    std::cout<<"Enter the num"<<std::endl;
+    int n;
+    std::cin>>n; 
+    switch (n){
+        case '1'...'2': 
+              std::cout<<"Winter"<<std::endl;      
+              break; 
+        case 12 :
+              std::cout<<"winter"<<std::endl;      
+            break;
+        case '3'...'5':
+              std::cout<<"spring"<<std::endl;
+             break;
+        case '6'...'8': 
+              std::cout<<"Summer"<<std::endl;      
+              break; 
+        case 9 :
+              std::cout<<"Fall"<<std::endl;      
+            break;
+        case 10:
+              std::cout<<"Fall"<<std::endl;      
+            break;
+        case 11 :
+              std::cout<<"Fall"<<std::endl;      
+            break;
+      default:
+             std::cout<<"error!"<<std::endl;
+}
+}
+void task_9(){
+    std::cout<<"Enter the number : "<<std::endl;
+    int n;
+    std::cin>>n;
+    switch (n){
+        case 1: 
+              std::cout<<"RED"<<std::endl;
+             break;
+        case 2: 
+              std::cout<<"ORANGE"<<std::endl;      
+              break; 
+        case 3:
+              std::cout<<"YELLOW"<<std::endl;      
+            break;
+        case 4:
+              std::cout<<"GREEN"<<std::endl;
+             break;
+        case 5: 
+              std::cout<<"NAIVE BLUE"<<std::endl;
+            break;
+        case 6: 
+              std::cout<<"BLUE"<<std::endl;
+                break;
+        case 7: 
+              std::cout<<"PURPULE"<<std::endl;
+                break;
+     
+       
+        default:
+             std::cout<<"error!"<<std::endl;
+    }
+}
+void task_10(){
+    std::cout<<"enter simbol"<<std::endl;
+    char a;
+    std::cin>>a;
+     switch(a){
+         case 'A'...'Z': 
+              std::cout<<"it's  bolshaya bukva"<<std::endl;
+             break;
+        case 'a'...'z': 
+              std::cout<<"it's malenki bukva"<<std::endl;      
+              break; 
+        case '1'...'9':
+              std::cout<<"chislo"<<std::endl;      
+            break;
+        
+       default:
+              std::cout<<"ERRooorrrrrrrr..........."<<std::endl;
+     }
+    
+    
+}
+    
+int main(){
+    
+task_1();
+task_3();
+task_4();
+task_5();
+task_6();
+task_7();
+task_8();
+task_9();
+task_10();
+
+    
+    
+    
+}
