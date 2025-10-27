8be9597a5b7e6e7a227c48f5b299c10171b8f65a
Lucykk1 <lusinekazaryan07@gmail.com>
2025-10-27 11:47:35 +0300

    Update 27.10.cpp

--- 27.10.cpp
+++ 27.10.cpp
@@ -68,3 +68,38 @@ std:wcin.imbue(std::locale("en_US.UTF-8"));
     }
     std::wcout<<std::endl<<std::endl;
     return 0;}
+3)
+    #include <iostream>
+#include <vector>
+#ifdef _WIN32
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <io.h>
+#include <fcntl.h>
+#endif
+
+
+int main()
+{
+#ifdef _WIN32
+    setlocale(LC_ALL,"");
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    _setmode(_fileno(stderr), _O_U16TEXT);
+    _setmode(_fileno(stdin), _O_U16TEXT);
+#else
+    std::ios_base::sync_with_stdio(false);
+std:wcout.imbue(std::locale("en_US.UTF-8"));
+std:wcin.imbue(std::locale("en_US.UTF-8"));
+#endif
+    int sum=0;
+    for(int i=1; i<=5; i++){
+        int num = 0 ;
+        std::wcout<<L"число "<<i<<L" "<<std::endl;
+        std::wcin>>num;
+        sum+=num;
+    }
+    std::wcout<<L"Сумма: "<<sum<<std::endl;
+    return 0;
+}
